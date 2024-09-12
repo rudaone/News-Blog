ced3bfb829e2673493d671c1b99253e2639e9e37
spadarbenj <makarchyk.b.a@gmail.com>
2024-09-12 12:45:39 +0300

    Add-sort

--- news-blog/src/components/Sort/Sort.tsx
+++ news-blog/src/components/Sort/Sort.tsx
@@ -4,7 +4,18 @@ import './Sort.css'
 const Sort = () => {
 
     return (
-        <div className="wrap">asdfghkl</div>
+        <div className="wrap">
+            <div className="wrap__sort-date">
+                <button className="sort-day">Day</button>
+                <button className="sort-week">Week</button>
+                <button className="sort-month">Month</button>
+                <button className="sort-year">Year</button>
+            </div>
+            <div className="sort__title">
+              <span style={{color:'#31303780'}}> Sort: <span>Title</span></span>
+                <select name=""></select>
+            </div>
+        </div>
     )
 
 }
